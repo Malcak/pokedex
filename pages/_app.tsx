6355a093ed8161d0b609e97828a085e90c2dc038
Malcak <malcakx@gmail.com>
2022-06-06 00:12:07 -0500

    feat(ui): :sparkles: implement light and dark themes
    added next-themes library for switching between themes, and added themas provider along with NextUI themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { darkTheme, lightTheme } from '../lib/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className
+      }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
-import { Button } from '@nextui-org/react'
+import { useTheme as useNextTheme } from 'next-themes'
+import { Switch, useTheme } from '@nextui-org/react'
 
 const Home: NextPage = () => {
+  const { setTheme } = useNextTheme();
+  const { isDark, type } = useTheme();
+
   return (
     <>
-      <Button color={'gradient'}>Click me</Button>
+      The current theme is: {type}
+      <Switch
+        checked={isDark}
+        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+      />
     </>
   )
 }
