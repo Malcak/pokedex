34201ecd653d7cc753cf682cdd8cad27815382b7
Malcak <malcakx@gmail.com>
2022-06-05 23:47:14 -0500

    feat(ui): :lipstick: add NextUI component library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
+import { Button } from '@nextui-org/react'
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <Button color={'gradient'}>Click me</Button>
     </>
   )
 }
