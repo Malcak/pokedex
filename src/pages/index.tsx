2ad42891ebf01c327670c8bd8c60b42de8b011c4
Malcak <malcakx@gmail.com>
2022-06-20 05:29:35 +0000

    feat(index): :sparkles: get pokemon info from static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,44 @@
-import type { NextPage } from 'next'
-import { useTheme as useNextTheme } from 'next-themes'
-import { Switch, useTheme } from '@nextui-org/react'
+import type { GetStaticProps, NextPage } from 'next'
 
-const Home: NextPage = () => {
-  const { setTheme } = useNextTheme()
-  const { isDark, type } = useTheme()
+import pokeapi from '@api/index'
+import { PokemonListItemResponse, PokemonListResponse } from 'interfaces'
 
+interface Props {
+  pokemons: PokemonListItemResponse[]
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <>
-      The current theme is: {type}
-      <Switch
-        checked={isDark}
-        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
-      />
+      <ul>
+        {pokemons.map(({ id, name }) => (
+          <li key={id}>
+            #{id} - {name}
+          </li>
+        ))}
+      </ul>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data } = await pokeapi.get<PokemonListResponse>('/pokemon?limit=151')
+
+  const pokemons: PokemonListItemResponse[] = data.results.map(
+    (pokemon, index) => ({
+      ...pokemon,
+      id: `${index + 1}`,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+        index + 1
+      }.svg`,
+    }),
+  )
+
+  return {
+    props: {
+      pokemons,
+    },
+  }
 }
 
 export default Home
