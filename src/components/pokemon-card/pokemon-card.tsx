3fbc16bf6db52205a7b9380d51a6caacbab67508
Malcak <malcakx@gmail.com>
2022-07-11 15:31:40 +0000

    feat(components): :sparkles: add link to each pokemon on click card

--- src/components/pokemon-card/pokemon-card.tsx
+++ src/components/pokemon-card/pokemon-card.tsx
@@ -1,4 +1,5 @@
 import type { FC } from 'react'
+import Link from 'next/link'
 import { Card, Grid, Row, Text } from '@nextui-org/react'
 
 import type { PokemonListItemResponse } from '@interfaces'
@@ -12,37 +13,39 @@ const PokemonCard: FC<Props> = ({ pokemon }) => {
 
   return (
     <Grid xs={6} sm={3} md={2} xl={1}>
-      <Card isHoverable isPressable variant="bordered">
-        <Card.Body
-          css={{
-            p: 1,
-          }}
-        >
-          <Card.Image
-            src={img}
-            width="100%"
-            height={140}
-            alt={name}
-            objectFit="contain"
-          />
-        </Card.Body>
-        <Card.Footer>
-          <Row wrap="wrap" justify="space-between" align="center">
-            <Text b transform="capitalize">
-              {name}
-            </Text>
-            <Text
-              css={{
-                color: '$accents7',
-                fontWeight: '$semibold',
-                fontSize: '$sm',
-              }}
-            >
-              # {id}
-            </Text>
-          </Row>
-        </Card.Footer>
-      </Card>
+      <Link href={`/pokemon/${id}`}>
+        <Card isHoverable isPressable variant="bordered">
+          <Card.Body
+            css={{
+              p: 1,
+            }}
+          >
+            <Card.Image
+              src={img}
+              width="100%"
+              height={140}
+              alt={name}
+              objectFit="contain"
+            />
+          </Card.Body>
+          <Card.Footer>
+            <Row wrap="wrap" justify="space-between" align="center">
+              <Text b transform="capitalize">
+                {name}
+              </Text>
+              <Text
+                css={{
+                  color: '$accents7',
+                  fontWeight: '$semibold',
+                  fontSize: '$sm',
+                }}
+              >
+                # {id}
+              </Text>
+            </Row>
+          </Card.Footer>
+        </Card>
+      </Link>
     </Grid>
   )
 }
