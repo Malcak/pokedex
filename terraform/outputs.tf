c5a4191bdc85d6a8d594c41e47af78225268dc0d
Malcak <malcakx@gmail.com>
2022-11-10 18:24:10 -0500

    ci: :construction_worker: add deploy step on ci

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,3 +15,11 @@ output "ecr_password" {
 output "load_balancer_ip" {
   value = aws_lb.default.dns_name
 }
+
+output "ecs_cluster_arn" {
+  value = aws_ecs_cluster.main.arn
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.pokedex.name
+}
