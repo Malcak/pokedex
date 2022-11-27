d434882bab0213d4b2cbdce606c5b08102d9d8d9
Malcak <malcakx@gmail.com>
2022-11-26 23:55:05 -0500

    test: :test_tube: fix failing tests

--- __tests__/components/pokemon-card.test.tsx
+++ __tests__/components/pokemon-card.test.tsx
@@ -17,10 +17,10 @@ describe('Pokemon Card Component', () => {
     expect(
       (await wrapper.findAllByRole('img', { name: bulbasaur.name })).at(0),
     ).toHaveAttribute('src', bulbasaur.img)
-    // the next one should fail
-    expect(
-      (await wrapper.findAllByRole('img', { name: bulbasaur.name })).at(0),
-    ).toHaveAttribute('src', bulbasaur.url)
+    // the next line should fail
+    // expect(
+    //   (await wrapper.findAllByRole('img', { name: bulbasaur.name })).at(0),
+    // ).toHaveAttribute('src', bulbasaur.url)
     expect(
       (await wrapper.findAllByRole('img', { name: bulbasaur.name })).at(0),
     ).toHaveAttribute('alt', bulbasaur.name)
