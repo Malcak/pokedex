71fe0d2207058ae5f1bad4d0055920214e217ddc
Malcak <malcakx@gmail.com>
2022-12-04 20:19:43 -0500

    DPL-25 ci: create iam instance profile

--- terraform/global/iam.tf
+++ terraform/global/iam.tf
@@ -1,4 +1,5 @@
-data "aws_iam_policy_document" "assume_role_policy" {
+# ecs task execution
+data "aws_iam_policy_document" "ecs_task_policy" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -11,10 +12,61 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "${var.project}-ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ecs instance
+
+data "aws_iam_policy_document" "ecs_instance_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name               = "${var.project}-ecs-instance-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_instance_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "${var.project}-ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.id
+}
+
+# ecs service
+
+data "aws_iam_policy_document" "ecs_service_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs-service-role" {
+  name               = "${var.project}-ecs-service-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_service_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_service_role_policy" {
+  role       = aws_iam_role.ecs-service-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
